18d8096a1a75314fe0cfd28792922761835c5878
Zhijie He <hezhijie0327@hotmail.com>
2021-02-26 15:16:46 +0800

    release.sh | Version 1.0.0
    1. <New> First upload.

---
+++ release.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Current Version: 1.0.0
+
+## How to get and use?
+# git clone "https://github.com/hezhijie0327/CNIPDb.git" && bash ./CNIPDb/release.sh
+
+## Function
+# Get Data
+function GetData() {
+    iana_extended=(
+        "https://ftp.iana.org/pub/mirror/rirstats/afrinic/delegated-afrinic-extended-latest"
+        "https://ftp.iana.org/pub/mirror/rirstats/apnic/delegated-apnic-extended-latest"
+        "https://ftp.iana.org/pub/mirror/rirstats/arin/delegated-arin-extended-latest"
+        "https://ftp.iana.org/pub/mirror/rirstats/lacnic/delegated-lacnic-extended-latest"
+        "https://ftp.iana.org/pub/mirror/rirstats/ripencc/delegated-ripencc-extended-latest"
+    )
+    iana_full=(
+        "https://ftp.iana.org/pub/mirror/rirstats/afrinic/delegated-afrinic-latest"
+        "https://ftp.iana.org/pub/mirror/rirstats/apnic/delegated-apnic-latest"
+        "https://ftp.iana.org/pub/mirror/rirstats/lacnic/delegated-lacnic-latest"
+        "https://ftp.iana.org/pub/mirror/rirstats/ripencc/delegated-ripencc-latest"
+    )
+    rm -rf ./cnipdb_* ./Temp && mkdir ./Temp && cd ./Temp
+    for iana_extended_task in "${!iana_extended[@]}"; do
+        curl -s --connect-timeout 15 "${iana_extended[$iana_extended_task]}" >> ./iana_extended.tmp
+    done
+    for iana_full_task in "${!iana_full[@]}"; do
+        curl -s --connect-timeout 15 "${iana_full[$iana_full_task]}" >> ./iana_full.tmp
+    done
+}
+# Analyse Data
+function AnalyseData() {
+    ipv4_data=($(cat ./iana_extended.tmp ./iana_full.tmp | grep "CN|ipv4" | sort | uniq | awk "{ print $2 }"))
+    ipv6_data=($(cat ./iana_extended.tmp ./iana_full.tmp | grep "CN|ipv6" | sort | uniq | awk "{ print $2 }"))
+}
+# Output Data
+function OutputData() {
+    for ipv4_data_task in "${!ipv4_data[@]}"; do
+        echo "$(echo $(echo ${ipv4_data[$ipv4_data_task]} | awk -F '|' '{ print $4 }')/$(echo ${ipv4_data[$ipv4_data_task]} | awk -F '|' '{ print 32 - log($5) / log(2) }') | grep -E '^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/(([0-9])|([1-2][0-9])|(3[0-2]){1,2})$')" >> ./cnipdb_ipv4.tmp
+    done
+    for ipv6_data_task in "${!ipv6_data[@]}"; do
+        echo "$(echo $(echo ${ipv6_data[$ipv6_data_task]} | awk -F '|' '{ print $4 }')/$(echo ${ipv6_data[$ipv6_data_task]} | awk -F '|' '{ print $5 }') | grep -E '^(([0-9a-f]{1,4}:){7,7}[0-9a-f]{1,4}|([0-9a-f]{1,4}:){1,7}:|([0-9a-f]{1,4}:){1,6}:[0-9a-f]{1,4}|([0-9a-f]{1,4}:){1,5}(:[0-9a-f]{1,4}){1,2}|([0-9a-f]{1,4}:){1,4}(:[0-9a-f]{1,4}){1,3}|([0-9a-f]{1,4}:){1,3}(:[0-9a-f]{1,4}){1,4}|([0-9a-f]{1,4}:){1,2}(:[0-9a-f]{1,4}){1,5}|[0-9a-f]{1,4}:((:[0-9a-f]{1,4}){1,6})|:((:[0-9a-f]{1,4}){1,7}|:)|fe80:(:[0-9a-f]{0,4}){0,4}%[0-9a-z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-f]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/(([0-9])|([1-9][0-9])|(1[0-1][0-9])|(12[0-8]){1,3})$')" >> ./cnipdb_ipv6.tmp
+    done
+    cat ./cnipdb_ipv4.tmp | sort | uniq > ../cnipdb_ipv4.txt && cat ./cnipdb_ipv6.tmp | sort | uniq > ../cnipdb_ipv6.txt && cat ../cnipdb_ipv4.txt ../cnipdb_ipv6.txt > ../cnipdb_combine.txt
+    cd .. && rm -rf ./Temp
+    exit 0
+}
+
+## Process
+# Call GetData
+GetData
+# Call AnalyseData
+AnalyseData
+# Call OutputData
+OutputData
